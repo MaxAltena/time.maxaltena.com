aad94b69b515d4308870516712e7ae85a5d98f61
MaxAltena <Max@MaxAltena.com>
2018-12-07 15:24:03 +0100

    started with building

---
+++ src/components/Content.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "../assets/react.svg";
+
+const Content = () => {
+  return (
+    <div className="Content">
+      <h1>time.MaxAltena.com</h1>
+      <img src={logo} alt="logo" />
+      <p>This is going to be my time related domain</p>
+    </div>
+  );
+};
+
+export default Content;
