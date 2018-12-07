664decf6f1cb355543bd60f60aed1aa4816ddd7a
MaxAltena <Max@MaxAltena.com>
2018-12-07 15:34:08 +0100

    ready to go

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,11 +1,10 @@
 import React from "react";
-import logo from "../assets/react.svg";
 
 const Content = () => {
   return (
     <div className="Content">
       <h1>time.MaxAltena.com</h1>
-      <img src={logo} alt="logo" />
+      <p>00:00:00</p>
       <p>This is going to be my time related domain</p>
     </div>
   );
